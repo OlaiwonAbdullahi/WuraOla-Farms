c14609b01fa30b6fb6f8c55646aba48f3edbbc0a
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 23:31:01 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import TopHeader from "./components/TopHeader";
 import Home from "./pages/home";
+import AboutPage from "./pages/AboutPage";
 
 const App = () => {
   return (
     <div>
-      <TopHeader />
-      <Navbar />
-      <Home />
+      <Router>
+        <TopHeader />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Router>
+
       <Footer />
     </div>
   );
