3c925d9d28fc2b25c49cd4c57d634d7005b76102
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 09:54:00 +0100

    update TopHeader.jsx

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -3,16 +3,18 @@ import { CiFacebook, CiLinkedin, CiMail } from "react-icons/ci";
 const TopHeader = () => {
   return (
     <div className="bg-secondary font-inter flex-col md:flex-row text-primary p-2 font-extralight flex justify-between px-4">
-      <div className=" flex gap-4 flex-col  md:flex-row">
-        <div className=" flex items-center gap-2 text-sm">
-          <PiMapPinAreaThin className=" w-5 h-5" />
-          Lorem ipsum dolor sit amet.
+      <div className=" flex md:gap-4 gap-2 flex-col  md:flex-row">
+        <div className=" flex gap-2">
+          <div className=" flex items-center gap-2 md:text-sm text-xs">
+            <PiMapPinAreaThin className=" w-5 h-5" />
+            Lorem ipsum dolor sit amet.
+          </div>
+          <div className=" flex items-center gap-2 md:text-sm text-xs">
+            <CiMail className=" w-5 h-5" />
+            Lorem ipsum dolor sit amet.
+          </div>
         </div>
-        <div className=" flex items-center gap-2 text-sm">
-          <CiMail className=" w-5 h-5" />
-          Lorem ipsum dolor sit amet.
-        </div>
-        <div className=" flex items-center gap-2 text-sm">
+        <div className=" flex items-center gap-2 md:text-sm text-xs">
           <PiPhoneThin className=" w-5 h-5" />
           +234(0)812 3456 789
         </div>
