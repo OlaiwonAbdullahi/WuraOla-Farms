4d40b1fa27484e2cfd670b567b2741e3349215fe
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 17:15:14 +0100

    update Getintouch.jsx

--- src/components/Getintouch.jsx
+++ src/components/Getintouch.jsx
@@ -2,13 +2,15 @@ import { FaLeaf } from "react-icons/fa";
 import Button from "./Button";
 const Getintouch = () => {
   return (
-    <div className=" flex justify-between">
-      <div className=" flex items-center gap-2">
-        <FaLeaf className=" h-10 w-10 text-secondary" />
-        Get in Touch
-      </div>
-      <div className="">
-        <Button>Get Started</Button>
+    <div className="bg-[url(/src/assets/cassava.jpeg)] bg-cover">
+      <div className=" flex md:justify-between flex-col md:flex-row justify-center mx-auto gap-5 p-3 bg-secondary  bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 h-52 ">
+        <div className=" flex items-center gap-2 text-3xl justify-center mx-auto font-kanit">
+          <FaLeaf className=" h-10 w-10 text-secondary" />
+          Get in Touch
+        </div>
+        <div className=" flex justify-center">
+          <Button>Get Started</Button>
+        </div>
       </div>
     </div>
   );
