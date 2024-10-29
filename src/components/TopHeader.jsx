137928ce47df035cb0b14f07d6bba6f6a5380a20
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 09:36:37 +0100

    update TopHeader.jsx

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -2,8 +2,8 @@ import { PiMapPinAreaThin, PiPhoneThin } from "react-icons/pi";
 import { CiFacebook, CiLinkedin, CiMail } from "react-icons/ci";
 const TopHeader = () => {
   return (
-    <div className="bg-secondary font-inter text-primary p-2 font-extralight flex justify-between px-4">
-      <div className=" flex gap-4 ">
+    <div className="bg-secondary font-inter flex-col md:flex-row text-primary p-2 font-extralight flex justify-between px-4">
+      <div className=" flex gap-4 flex-col  md:flex-row">
         <div className=" flex items-center gap-2 text-sm">
           <PiMapPinAreaThin className=" w-5 h-5" />
           Lorem ipsum dolor sit amet.
