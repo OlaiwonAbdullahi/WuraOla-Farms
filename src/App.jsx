e0c330fded34ccb3bbf449179a35cada86a0a46b
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 10:56:26 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import TopHeader from "./components/TopHeader";
 
@@ -6,6 +7,7 @@ const App = () => {
     <div>
       <TopHeader />
       <Navbar />
+      <Hero />
     </div>
   );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div className=" bg-[url(/src/assets/Hero.jpg)] p-4">
+      <div className="">asdfghjk</div>
+    </div>
+  );
+};
+
+export default Hero;
