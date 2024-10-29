4833439fc5533d9c825b989d9ce5796d4cecf8b2
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 10:00:16 +0100

    update TopHeader.jsx

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -2,26 +2,38 @@ import { PiMapPinAreaThin, PiPhoneThin } from "react-icons/pi";
 import { CiFacebook, CiLinkedin, CiMail } from "react-icons/ci";
 const TopHeader = () => {
   return (
-    <div className="bg-secondary font-inter flex-col md:flex-row text-primary p-2 font-extralight flex justify-between px-4">
+    <div className="bg-secondary font-inter gap-2 flex-col md:flex-row text-primary p-2 font-extralight flex justify-between px-4">
       <div className=" flex md:gap-4 gap-2 flex-col  md:flex-row">
         <div className=" flex gap-2">
-          <div className=" flex items-center gap-2 md:text-sm text-xs">
+          <div className=" flex items-center gap-2 md:text-sm text-xs whitespace-nowrap">
             <PiMapPinAreaThin className=" w-5 h-5" />
-            Lorem ipsum dolor sit amet.
+            Lorem ipsum dolor si
           </div>
-          <div className=" flex items-center gap-2 md:text-sm text-xs">
+          <div className=" flex items-center gap-2 md:text-sm text-xs whitespace-nowrap">
             <CiMail className=" w-5 h-5" />
             Lorem ipsum dolor sit amet.
           </div>
         </div>
-        <div className=" flex items-center gap-2 md:text-sm text-xs">
+
+        <div className=" md:flex items-center gap-2 md:text-sm text-xs hidden">
           <PiPhoneThin className=" w-5 h-5" />
           +234(0)812 3456 789
         </div>
       </div>
-      <div className=" flex gap-2">
+      <div className=" md:flex hidden gap-2">
         <CiLinkedin className=" h-6 w-6" />
         <CiFacebook className=" h-6 w-6" />
+      </div>
+
+      <div className=" flex md:hidden justify-between">
+        <div className=" flex items-center gap-2 md:text-sm text-xs">
+          <PiPhoneThin className=" w-5 h-5" />
+          +234(0)812 3456 789
+        </div>
+        <div className=" flex gap-2">
+          <CiLinkedin className=" h-6 w-6" />
+          <CiFacebook className=" h-6 w-6" />
+        </div>
       </div>
     </div>
   );
