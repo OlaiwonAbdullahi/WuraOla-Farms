66dfb1b2cdfbad12faee4b9c14993f00bb349126
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-29 17:30:48 +0100

    update Button.jsx and Getintouch.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ children }) => {
   return (
-    <div className="bg-secondary p-2 gap-2 whitespace-nowrap md:text-base  font-inter text-sm text-primary rounded-b-full rounded-e-full md:w-40 w-36 flex justify-center items-center">
+    <div className="bg-secondary p-2 gap-2 whitespace-nowrap md:text-base shadow-2xl font-inter text-sm text-primary rounded-b-full rounded-e-full md:w-40 w-36 flex justify-center items-center">
       {children}
     </div>
   );
--- src/components/Getintouch.jsx
+++ src/components/Getintouch.jsx
@@ -2,13 +2,13 @@ import { FaLeaf } from "react-icons/fa";
 import Button from "./Button";
 const Getintouch = () => {
   return (
-    <div className="bg-[url(/src/assets/cassava.jpeg)] bg-cover">
-      <div className=" flex md:justify-between flex-col md:flex-row justify-center mx-auto gap-5 p-3 bg-secondary  bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 h-52 ">
-        <div className=" flex items-center gap-2 text-3xl justify-center mx-auto font-kanit">
-          <FaLeaf className=" h-10 w-10 text-secondary" />
+    <div className="bg-[url(/src/assets/cassava.jpeg)] bg-cover text-primary bg-center bg-primary bg-no-repeat rounded-t-2xl md:rounded-none">
+      <div className=" flex md:justify-between flex-col md:flex-row justify-center items-center mx-auto gap-5 p-3 bg-secondary rounded-t-3xl md:rounded-none bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 h-52 ">
+        <div className=" flex items-center gap-2 md:text-5xl text-3xl justify-center md:justify-start  font-kanit">
+          <FaLeaf className=" h-10 w-10 md:size-14 text-primary" />
           Get in Touch
         </div>
-        <div className=" flex justify-center">
+        <div className=" flex justify-center  h-16 items-center">
           <Button>Get Started</Button>
         </div>
       </div>
