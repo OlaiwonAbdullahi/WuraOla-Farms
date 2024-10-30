9061ef8d4702e3466dd7232a4630ddfda9c63231
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-30 11:38:21 +0100

    update AboutHeader.jsx

--- src/components/AboutHeader.jsx
+++ src/components/AboutHeader.jsx
@@ -1,12 +1,14 @@
 const AboutHeader = () => {
   return (
     <div className="bg-[url(/src/assets/abouthero.jpeg)] bg-cover bg-center flex gap-2 flex-col justify-center bg-primary bg-no-repeat md:h-[50vh] h-[40vh]">
-      <h2 className=" text-primary text-4xl text-center font-kanit">
-        About Us
-      </h2>
-      <span className="text-primary text-center font-inter">
-        Home | | About Us
-      </span>
+      <div className=" flex flex-col gap-3 bg-black md:h-[50vh] h-[40vh] justify-center bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-15">
+        <h2 className=" text-primary text-4xl text-center font-kanit">
+          About Us
+        </h2>
+        <span className="text-primary text-center font-inter">
+          Home | | About Us
+        </span>
+      </div>
     </div>
   );
 };
