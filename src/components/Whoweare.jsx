962d6290daab74e0faa9e4198b5d53ded01a7ff6
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-30 18:21:38 +0100

    update Whoweare.jsx

--- src/components/Whoweare.jsx
+++ src/components/Whoweare.jsx
@@ -5,13 +5,18 @@ const Whoweare = () => {
         <h2 className=" text-secondary font-kanit font-semibold text-2xl border-l-4 px-2 border-green-300">
           About Us
         </h2>
-        <span className=" text-4xl font-kanit text-secondary">Who We Are</span>
+        <span className=" p-3 text-4xl font-kanit text-secondary">
+          Who We Are
+        </span>
       </div>
-      <div className="basis-2/3 text-center md:text-left font-inter">
+      <div className="basis-2/3 font-light text-center md:text-left font-inter">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia dolore
         exercitationem repellat ipsa officiis quia doloribus possimus officia
         perspiciatis ab sunt suscipit fuga dolores consectetur voluptates atque,
-        dicta quisquam voluptate.
+        dicta quisquam voluptate. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. At iste neque recusandae nam, quia facere, autem non
+        exercitationem perferendis atque a? Quis eligendi in modi expedita
+        necessitatibus ut iste aut?
       </div>
     </div>
   );
