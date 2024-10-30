9a81155ed4ee2f921aa13c4d5d3477f4704e9b82
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-30 12:18:10 +0100

    update AboutPage.jsx

---
+++ src/components/Whoweare.jsx
@@ -0,0 +1,14 @@
+const Whoweare = () => {
+  return (
+    <div>
+      <div className="">
+        <h2 className=" text-secondary font-kanit font-semibold text-2xl border-l-4 px-2 border-green-300">
+          Who We Are
+        </h2>
+      </div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Whoweare;
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,14 @@
 import AboutHeader from "../components/AboutHeader";
+import Whoweare from "../components/Whoweare";
 
 const AboutPage = () => {
   return (
     <div>
       <div className="">
         <AboutHeader />
+      </div>
+      <div className="">
+        <Whoweare />
       </div>
     </div>
   );
