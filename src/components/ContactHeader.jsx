c93506de9d69a5fd1e4d91a4c2b166c30fa07939
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-30 23:02:48 +0100

    update ContactHeader.jsx

--- src/components/ContactHeader.jsx
+++ src/components/ContactHeader.jsx
@@ -1,7 +1,7 @@
 const ContactHeader = () => {
   return (
     <div className="bg-[url(/src/assets/field.jpeg)] bg-cover bg-center flex gap-2 flex-col justify-center bg-primary bg-no-repeat md:h-[50vh] h-[40vh]">
-      <div className=" flex flex-col gap-3 bg-black md:h-[50vh] h-[40vh] justify-center bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-15">
+      <div className=" flex flex-col gap-3 bg-black md:h-[50vh] h-[40vh] justify-center bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-5">
         <h2 className=" text-primary text-4xl text-center font-kanit">
           Contact Us
         </h2>
