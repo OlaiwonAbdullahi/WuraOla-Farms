88b5fcff896ceff53bd7a5f71ea3394199165d96
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-30 18:19:51 +0100

    update Whoweare.jsx

--- src/components/Whoweare.jsx
+++ src/components/Whoweare.jsx
@@ -3,8 +3,9 @@ const Whoweare = () => {
     <div className=" flex justify-around p-4 w-4/5 mx-auto flex-col md:flex-row">
       <div className=" basis-1/3">
         <h2 className=" text-secondary font-kanit font-semibold text-2xl border-l-4 px-2 border-green-300">
-          Who We Are
+          About Us
         </h2>
+        <span className=" text-4xl font-kanit text-secondary">Who We Are</span>
       </div>
       <div className="basis-2/3 text-center md:text-left font-inter">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia dolore
