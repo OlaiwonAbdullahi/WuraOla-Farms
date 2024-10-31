bba8ed11dee98a23f2f3e266c0f2dfb4ce50685b
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-31 18:58:17 +0100

    build(deps): update package-lock.json, package.json and Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,15 @@
 import Button from "./Button";
 import { GoArrowRight } from "react-icons/go";
-
+import "animate.css";
 const Hero = () => {
   return (
     <div className="bg-[url(/src/assets/Hero.jpg)] bg-cover bg-center bg-primary bg-no-repeat md:h-[50vh] h-[40vh]">
       <div className="flex flex-col gap-4 p-6">
-        <h2 className="md:text-5xl leading-relaxed whitespace-nowrap md:leading-snug text-4xl text-center md:text-left text-primary font-kanit">
+        <h2 className="md:text-5xl animate__lightSpeedInLeft leading-relaxed whitespace-nowrap md:leading-snug text-4xl text-center md:text-left text-primary font-kanit">
           Nourishing the Land,
           <br /> Feeding the Future
         </h2>
-        <p className="leading-snug font-light w-full max-w-lg text-white text-sm font-inter text-center md:text-left flex mx-auto md:mx-1">
+        <p className="leading-snug font-light w-full animate__lightSpeedOutRight max-w-lg text-white text-sm font-inter text-center md:text-left flex mx-auto md:mx-1">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui saepe
           consectetur neque nisi reprehenderit architecto?
         </p>
